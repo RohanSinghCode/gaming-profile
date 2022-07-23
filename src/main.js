21b1e0db5146b8dcbb7159a1b18dfa4a11d6cd3a
Rohan Singh <rohan.s@deltax.com>
2022-07-24 01:16:23 +0530

    add vuetify library

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import vuetify from "./plugins/vuetify";
 
-createApp(App).use(router).mount("#app");
+new Vue({
+  router,
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
